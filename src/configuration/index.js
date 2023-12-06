124bc7a7b1fb759576260a5d1c11f04776f4ce02
oilliever <baduy.work@gmail.com>
2023-12-06 20:05:02 +0700

    update env

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -2,9 +2,8 @@ import environmentConfig from './enviromentConfig';
 import { storageKey } from './storageKeys';
 
 const isDevEnv = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-console.log(isDevEnv);
 const envConfig = isDevEnv ? environmentConfig.development : environmentConfig.production;
-const endPointAdmin = 'https://localhost:7297';
+const endPointAdmin = 'http://oilliever-001-site1.atempurl.com/';
 const Config = {
     isDevEnv,
     storageKey,
